d345a37bfce7b7e828333547a161517d85cee10d
YeonginKim <fhtmah12@gmail.com>
2022-06-23 23:33:31 +0900

    UserRepository Interface created

---
+++ src/main/java/com/webboard/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.webboard.repository;
+
+import com.webboard.model.ConnectedUser;
+import com.webboard.model.User;
+import com.webboard.model.commons.Id;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository {
+
+    User insert(User user);
+
+    void update(User user);
+
+    Optional<User> findById(Id<User, Long> userId);
+
+    Optional<ConnectedUser> findAllConnectedUser(Id<User, Long> userId);
+
+    List<Id<User, Long>> findConnectedIds(Id<User, Long> userId);
+}
